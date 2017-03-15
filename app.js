241868fa920bad5101d8b6e4e81cdde8b1fe46ff
Avinash Anantharamu <avinashanantharamu@Avinashs-MacBook-Pro.local>
2017-03-14 23:21:12 -0700

    scraping commit

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var express = require('express');
 var request = require('request');
 var fs = require('fs');
 var app = express();
-var port = 8080;
+var port = 8081;
 
-//example1
+//example1 - google.com output in html 
 var url="http://google.com";
 request(url,function(err,resp,body){
    if(err) {
@@ -14,6 +14,13 @@ request(url,function(err,resp,body){
     }   
    
 });
+
+//example2  - save google.com locally
+var url= "http://google.com";
+var destination = fs.createWriteStream('./downloads/google2.html');
+request(url)
+.pipe(destination);
+ 
 app.listen(port,function(){
   console.log('app listening on port' + port );
 }) 
